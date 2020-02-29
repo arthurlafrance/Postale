006a9acff2d4124576b766ed3de9a03237236da3
Arthur Lafrance <lafrancearthur@gmail.com>
2020-02-29 03:08:55 -0800

    finished db functions

--- postale/db.py
+++ postale/db.py
@@ -15,63 +15,16 @@ _TABLES = {
         'password' : 'VARCHAR(50)'
     },
     'message' : {
-        'sender' : 'VARCHAR(100)',
+        'sender' : 'VARCHAR(100)', # also have to store mailbox foreign key/rowid
         'subject' : 'VARCHAR(250)',
         'content' : 'TEXT(5000)',
+        'is_draft' : 'INTEGER'
     },
     'recipient' : {
         'message' : 'INTEGER',
         'address' : 'VARCHAR(100)',
     },
 }
-
-
-# Accounts
-def get_mailboxes(cursor: sqlite3.Cursor, url='', addr='', password='') -> [models.Mailbox]:
-    '''Get a list of all mailboxes'''
-    
-    cursor.execute('SELECT * FROM mailbox') # add WHERE
-    mailboxes = []
-
-    for row in cursor.fetchall():
-        mailboxes.append(models.Mailbox.from_data(row))
-
-    return mailboxes
-
-
-def add_mailbox(cursor: sqlite3.Cursor, mailbox: models.Mailbox):
-    '''Add a mailbox to the database'''
-    
-    query = 'INSERT INTO mailbox(' + ', '.join(column for column in _TABLES['mailbox']) + ') VALUES (?, ?, ?);'
-    cursor.execute(query, mailbox.export())
-
-
-def remove_mailbox(cursor: sqlite3.Cursor, mailbox: models.Mailbox):
-    '''Remove a mailbox from the database'''
-    pass
-
-
-
-# Messages/drafts
-def get_messages(cursor: sqlite3.Cursor, *mailboxes: [str]) -> [models.Message]:
-    '''Get all stored messages from the given mailbox(es)'''
-    pass
-
-
-def select_messages(cursor: sqlite3.Cursor, criteria_goes_here) -> [models.Message]: # (with criteria)
-    '''Select all stored messages from the given mailbox(es) matching the given criteria'''
-    pass
-
-
-def save_messages(cursor: sqlite3.Cursor, messages: [models.Message]): # (or update_messages)
-    '''Save the given messages to the database (or update them in the database if they exist)'''
-    pass 
-
-
-def save_message(cursor: sqlite3.Cursor, message: models.Message): # saves new message or updates existing one
-    '''Save the given message to the database, or update the existing message entry if it exists'''
-    pass
-
 
 
 # Logistics
@@ -113,3 +66,108 @@ def _get_schema_for_table(cursor: sqlite3.Cursor, table: str) -> {str : str}:
     
     cursor.execute(f'PRAGMA TABLE_INFO({table});')
     return {schema[1] : schema[2] for schema in cursor.fetchall()}
+
+
+    
+# Mailboxes
+def get_mailboxes(cursor: sqlite3.Cursor, id=-1, url='', addr='', password='') -> [models.Mailbox]:
+    '''Get a list of all mailboxes'''
+    
+    cursor.execute('SELECT rowid, * FROM mailbox') # add WHERE
+    rows = cursor.fetchall()
+
+    mailboxes = []
+
+    for row in rows:
+        mailboxes.append(models.Mailbox.from_data(row[0], row[1:]))
+
+    return mailboxes
+
+
+def get_mailbox_with_id(cursor: sqlite3.Cursor, id=-1) -> models.Mailbox:
+    '''Get the mailbox with the given ID (a convenience method wrapping around get_mailboxes)'''
+
+    result = get_mailboxes(cursor, id=id)
+    return result[0] if len(result) > 0 else None
+
+
+def write_mailbox(cursor: sqlite3.Cursor, mailbox: models.Mailbox):
+    '''Add a mailbox to the database'''
+    
+    query = 'INSERT INTO mailbox(' + ', '.join(_TABLES['mailbox']) + ') VALUES (' + ', '.join(['?'] * len(_TABLES['mailbox'])) + ');'
+    cursor.execute(query, mailbox.export())
+
+
+def delete_mailbox(cursor: sqlite3.Cursor, mailbox: models.Mailbox):
+    '''Remove a mailbox from the database'''
+    
+    query = 'DELETE FROM mailbox WHERE rowid=?;'
+    cursor.execute(query, (mailbox.id,))
+
+
+
+# Messages
+def get_messages(cursor: sqlite3.Cursor, mailboxes=None, criteria_goes_here=None) -> [models.Message]:
+    '''Get all stored messages from the given mailbox(es) matching the given criteria'''
+
+    cursor.execute('SELECT rowid, * FROM message;') # add WHERE
+    rows = cursor.fetchall()
+
+    messages = []
+
+    for row in rows:
+        message_id = row[0]
+        recipients = _get_recipients_for_message(cursor, message_id)
+
+        messages.append(models.Message.from_data(message_id, row[1:], recipients))
+
+    return messages
+
+def write_messages(cursor: sqlite3.Cursor, messages: [models.Message]): # (or update_messages)
+    '''Save the given messages to the database (or update them in the database if they exist)'''
+    
+    for message in messages:
+        write_message(cursor, message)
+
+
+def write_message(cursor: sqlite3.Cursor, message: models.Message): # saves new message or updates existing one
+    '''Save the given message to the database, or update the existing message entry if it exists'''
+    
+    insert_query = 'INSERT INTO message(' + ', '.join(_TABLES['message']) + ') VALUES(' + ', '.join(['?'] * len(_TABLES['message'])) + ');'
+    cursor.execute(insert_query, message.export())
+
+    _write_recipients_for_message(cursor, message.id, message.recipients)
+
+
+def delete_message(cursor: sqlite3.Cursor, message: models.Message):
+    '''Delete the given message from the database'''
+    
+    query = 'DELETE FROM message WHERE rowid=?;'
+    cursor.execute(query, (message.id,))
+
+    _delete_recipients_for_message(cursor, message.id)
+
+
+def _get_recipients_for_message(cursor: sqlite3.Cursor, message_id: int) -> [str]:
+    '''Get a list of all recipients for the message with the given ID'''
+    
+    cursor.execute('SELECT address FROM recipient WHERE message=?;', (message_id,))
+    return [row[0] for row in cursor.fetchall()]
+
+
+def _write_recipients_for_message(cursor: sqlite3.Cursor, message_id: int, recipients: [str]):
+    '''Write the recipient list into the database'''
+
+    insert_query = 'INSERT INTO recipient(' + ', '.join(_TABLES['recipient']) + ') VALUES(' + ', '.join(['?'] * len(_TABLES['recipient'])) + ');'
+    
+    for recipient in recipients:
+        cursor.execute(insert_query, (message_id, recipient))
+
+
+def _delete_recipients_for_message(cursor: sqlite3.Cursor, message_id: int):
+    '''Delete all recipients for the message with the given ID from the database'''
+
+    query = 'DELETE FROM recipient WHERE message=?;'
+    cursor.execute(query, (message_id,))
+
+
--- postale/mailman/models.py
+++ postale/mailman/models.py
@@ -5,9 +5,10 @@
 
 
 class Mailbox:
-    def __init__(self, url: str, addr: str, password: str):
-        self.url = url
+    def __init__(self, id: int, url: str, addr: str, password: str):
+        self.id = id
 
+        self.url = url
         self.addr = addr
         self.password = password
 
@@ -18,22 +19,27 @@ class Mailbox:
 
     def __eq__(self, mailbox) -> bool:
         if type(mailbox) == Mailbox:
-            return self.url == mailbox.url and self.addr == mailbox.addr and self.password == mailbox.password
+            return self.id == mailbox.id and self.export() == mailbox.export()
         else:
             return NotImplemented
 
     def __str__(self) -> str:
         return f'{self.url}: {self.addr} - {self.password}'
 
+    def __repr__(self) -> str:
+        return f'Mailbox({self.id}, {self.url}, {self.addr}, {self.password})'
+
     @staticmethod
-    def from_data(data: tuple):
+    def from_data(id: int, data: tuple):
         url, addr, password = data
         
-        return Mailbox(url, addr, password)
+        return Mailbox(id, url, addr, password)
 
 
 class Message:
-    def __init__(self, sender: str, recipients: [str], subject: str, content: str, is_draft=False):
+    def __init__(self, id: int, sender: str, recipients: [str], subject: str, content: str, is_draft=False):
+        self.id = id
+
         self.sender = sender
         self.recipients = recipients
 
@@ -42,8 +48,22 @@ class Message:
 
         self.is_draft = is_draft
 
+    def export(self) -> tuple:
+        '''Export the Message to a tuple of field values, ready to be used in SQL'''
+
+        return (self.sender, self.subject, self.content, 1 if self.is_draft else 0)
+
+    def __eq__(self, message) -> bool:
+        if type(message) == Message:
+            return self.id == message.id and self.export() == message.export() and self.recipients == message.recipients
+        else:
+            return NotImplemented
+
+    def __repr__(self) -> str:
+        return f'Message({self.id}, {repr(self.sender)}, {self.recipients}, {repr(self.subject)}, {repr(self.content)}, {self.is_draft})'
+
     @staticmethod
-    def from_data(data: tuple, recipients: [str]):
+    def from_data(id: int, data: tuple, recipients: [str]):
         sender, subject, content, is_draft = data
         
-        return Message(sender, recipients, subject, content, is_draft)
+        return Message(id, sender, recipients, subject, content, is_draft == 1)
